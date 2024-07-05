d3ef24376abba75125bb6729e89b71418ee3ec9a
Luiz Carlos Cosmi Filho <luizcarloscosmifilho@gmail.com>
2024-07-05 20:15:11 -0300

    fix: training for scratch all fc layers in vgg19 model

--- project/models/vgg19.py
+++ project/models/vgg19.py
@@ -7,12 +7,18 @@ class CustomVGG19(nn.Module):
     def __init__(self, num_class: int) -> None:
         super().__init__()
         self.model_conv = vgg19(weights="IMAGENET1K_V1")
-        for param in self.model_conv.parameters():
+        for param in self.model_conv.features.parameters():
             param.requires_grad = False
-        number_features = self.model_conv.classifier[6].in_features
-        features = list(self.model_conv.classifier.children())[:-1]
-        features.extend([nn.Linear(number_features, num_class)])
-        self.model_conv.classifier = nn.Sequential(*features)
+
+        self.model_conv.classifier = nn.Sequential(
+            nn.Linear(512 * 7 * 7, 4096),
+            nn.ReLU(True),
+            nn.Dropout(p=0.5),
+            nn.Linear(4096, 4096),
+            nn.ReLU(True),
+            nn.Dropout(p=0.5),
+            nn.Linear(4096, num_class),
+        )
         self.soft = nn.Softmax(dim=-1)
 
     def forward(self, x, softmax: bool = False):
